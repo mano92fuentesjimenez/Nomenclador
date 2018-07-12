99f0c1306311e2b4a372a243b973969510c29834
mano <manuel@uct.geocuba.cu>
2018-07-12 19:12:02 -0400

    Adicionando campo table

---
+++ htdocs/js/types/nomenclador.types.table.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Mano on 10/05/2017.
+ */
+(function(){
+	var nom = AjaxPlugins.Nomenclador,
+		addType =nom.Type.Utils.addType;
+
+	addType('Db_Table',Ext.extend(nom.Type.ValueType, {
+		nameToShow :"Tabla",
+		getValueEditExtComp :function (enumInstance, field){
+			return new AjaxPlugins.Ext3_components.fields.simpleField({
+				allowBlank :!field.needed,
+				fieldLabel :field.header
+			})
+		},
+        getPropertiesExtComp :function (enumInstance,_enumId, fieldId, fields){
+			var f = function(){
+			   var args = [].slice.call(arguments);
+			};
+          	var creator = new nom.nomencladorCreator({
+				enumInstance:enumInstance,
+				entityType: 'Tabla',
+				tplConfigs:{
+					default:{
+						defaultFields:{},
+						dataTypes:{
+							'DB_Bool':true,
+							'DB_String':true,
+
+							'DB_Number':true,
+							'DB_Description':true,
+						}
+					}
+				},
+                listeners: {
+                    "finishedCreation":f,
+                    'close':  function() {
+                        this.fireEvent('propertynotsetted');
+                    }
+                },
+			});
+			return creator;
+        },
+	}));
+
+})();
