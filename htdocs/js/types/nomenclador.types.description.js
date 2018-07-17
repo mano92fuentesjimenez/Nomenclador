5eb6b41e4cf15be2379a85474aa721d9a5e08dea
mano <manuel@uct.geocuba.cu>
2018-07-17 00:03:15 -0400

    cambiado la manera de crecer de descripcion. Ahora es un plugin.

--- htdocs/js/types/nomenclador.types.description.js
+++ htdocs/js/types/nomenclador.types.description.js
@@ -3,8 +3,10 @@
  */
 (function(){
     var nom = AjaxPlugins.Nomenclador,
-        addw = AjaxPlugins.Ext3_components.Windows.AddModWindow,
-        fields = AjaxPlugins.Ext3_components.fields,
+		comps = AjaxPlugins.Ext3_components,
+		addw = comps.Windows.AddModWindow,
+        fields = comps.fields,
+		plugins = comps.plugins,
         addType =nom.Type.Utils.addType;
 
     addType('DB_Description',Ext.extend(nom.Type.ValueType, {
@@ -13,56 +15,59 @@
             var fld = new fields.fieldDescripcion({
                 allowBlank :!field.needed,
                 fieldLabel :field.header,
-                height:100
+                height:100,
+				plugins:[
+					new grow()
+				]
             });
-            var id = ('desc')._id_();
 
-            nom.Type.Types.DB_Description.instances[id]=fld;
 
-            fld.on('afterrender',function(){
-
-                var wrap = fld.el.wrap({
-                    children:[{
-                        cls:'enum_desc_button',
-                        onclick:'AjaxPlugins.Nomenclador.Type.Types.DB_Description.showDescription("'+id+'")'
-                    }]
-                });
-                fld.wrap = wrap;
-                wrap.addClassOnOver('enum_desc_hover');
-
-            });
-            fld.on('destroy',function () {
-                delete nom.Type.TYpes.DB_Description.instances[id];
-            });
             return fld;
         }
-    })._apply_({
-        instances:{},
-        showDescription:function(instance){
-            var field = nom.Type.Types.DB_Description.instances[instance];
-
-            var textA = new fields.fieldDescripcion({
-                fieldLabel:field.fieldLabel,
-                allowBlank: field.allowBlank
-            });
-            textA.setValue(field.getValue());
-            var w = new addw({
-                width:600,
-                height:600,
-                layout:'fit',
-                title:field.fieldLabel,
-                items:[textA],
-                fields:{
-                    value:textA
-                },
-                hideApplyButton:true,
-                callback:function(d){
-                    field.setValue(d.all.value)
-                },
-                modal:true
-            });
-            w.show();
-        }
     }));
+
+	var grow = Ext.extend(function(){},{
+		init:function(fld){
+			var id = ('gis_grow')._id_();
+			fld.on('afterrender',function(){
+
+				var wrap = fld.el.wrap({
+					children:[{
+						id: id,
+						cls:'gis_grow_button'
+					}]
+				});
+				fld.wrap = wrap;
+				var el =Ext.Element.get(id).dom;
+
+				el.addEventListener('click',function(){
+					var textA = new fields.fieldDescripcion({
+						fieldLabel:fld.fieldLabel,
+						allowBlank: fld.allowBlank
+					});
+					textA.setValue(fld.getValue());
+					var w = new addw({
+						width:600,
+						height:600,
+						layout:'fit',
+						title:fld.fieldLabel,
+						items:[textA],
+						fields:{
+							value:textA
+						},
+						hideApplyButton:true,
+						callback:function(d){
+							fld.setValue(d.all.value)
+						},
+						modal:true
+					});
+					w.show();
+				});
+
+				wrap.addClassOnOver('gis_grow_hover');
+
+			});
+		}
+	})
 
 })();
