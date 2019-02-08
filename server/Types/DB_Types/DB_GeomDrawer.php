d3490c7e9dd025d294bddab4d268bd8d70b44bcc
Manuel Fuentes <mano92@nauta.cu>
2019-02-07 20:26:30 -0500

    modified geomDrawer so geometry is stored in wkb and retrieved as wkb

--- server/Types/DB_Types/DB_GeomDrawer.php
+++ server/Types/DB_Types/DB_GeomDrawer.php
@@ -10,7 +10,23 @@ class DB_GeomDrawer extends BaseType{
 
     public static function getDBTypeCreation($enumInstance, $connType,$typeProperties, $enum)
     {
-        return "text";
+        return 'geometry';
+//        switch ($typeProperties) {
+//            case 'Line':
+//                return 'geometry';
+//            case 'Point':
+//                return 'geometry(point,4326)';
+//            case 'Polygon':
+//                return 'geometry';
+//        }
+    }
+    public static  function transformSelect($fieldPath){
+        return "ST_AsGeoJSON($fieldPath)";
+    }
+    public static function getValueToDB($record, $value, $field, $connType){
+        if(is_null($value) or $value === '')
+            return 'null';
+        return "ST_GeomFromGeoJSON('$value')";
     }
 
 }
